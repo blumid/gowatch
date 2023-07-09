1ae2c7aaefa0219b179d58b0c7bfe448ef3b8d46
blumid <shhapip13@gmail.com>
2023-07-09 16:43:53 +0330

    before modifying operations.go - Updated()

--- db/operations.go
+++ db/operations.go
@@ -1,12 +1,14 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"github.com/blumid/gowatch/structure"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func AddProgram(program *structure.Program) error {
@@ -56,4 +58,37 @@ func FindProgram(filter interface{}) bool {
 
 func CheckScope() {
 
+}
+
+func Updated() interface{} {
+	// Define the search criteria
+	filter := bson.M{"_id": "document_id"}
+
+	// Define the update
+	update := bson.M{"$addToSet": bson.M{"myArrayField": bson.M{"$each": []string{"new_item1", "new_item2"}}}}
+
+	// Execute the update
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		panic(err)
+	}
+	// Retrieve the added items
+	projection := bson.M{"myArrayField": bson.M{"$slice": bson.A{-result.ModifiedCount, result.ModifiedCount}}}
+	cursor, err := collection.Find(context.Background(), filter, options.Find().SetProjection(projection))
+	if err != nil {
+		panic(err)
+	}
+
+	// Iterate over the results and print out the added items
+	for cursor.Next(context.Background()) {
+		var document bson.M
+		err := cursor.Decode(&document)
+		if err != nil {
+			panic(err)
+		}
+		addedItems := document["myArrayField"].([]interface{})[:result.ModifiedCount]
+		fmt.Println("Added items:", addedItems)
+	}
+
+	return ""
 }
