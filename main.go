ad343699f8a7a47d467f68d05ab773230a78e887
blumid <shhapip13@gmail.com>
2024-03-28 16:45:25 +0330

    renew - day1

--- main.go
+++ main.go
@@ -1,71 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"os/signal"
 	"syscall"
 
 	"os"
 
-	"github.com/blumid/gowatch/db"
-	"github.com/blumid/gowatch/discord"
-	"github.com/blumid/gowatch/structure"
+	"github.com/blumid/gowatch/tasks"
 )
 
 func main() {
-	// connect discord bot:
-	discord.Connect()
+
 
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 	sig := <-sigchan
 	fmt.Println("Received signal:", sig)
 
-	process()
+	//start tasks
+	tasks.Start()
 
-}
-
-func process() {
-
-	file, err := os.ReadFile("temp.json")
-
-	if err != nil {
-		fmt.Println("err in opening file: ", err)
-	}
-
-	var temp []structure.Program
-	err2 := json.Unmarshal(file, &temp)
-	if err2 != nil {
-		fmt.Println("err2 is: ", err2)
-	}
-
-	for _, v := range temp {
-
-		res := db.FandU(v.Name, v.Target.InScope)
-		if !res {
-			if err := db.AddProgram(&v); err != nil {
-				fmt.Println("new one add: ", v.Name)
-			}
-
-		}
-
-	}
-
-}
-
-func download() {
-	res, err := http.Get("https://github.com/arkadiyt/bounty-targets-data/blob/main/data/hackerone_data.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer res.Body.Close()
-	_, err2 := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err2)
-	}
 }
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,1 +1,67 @@
 package tasks
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/blumid/gowatch/db"
+	"github.com/blumid/gowatch/discord"
+	"github.com/blumid/gowatch/structure"
+)
+
+func Start() {
+
+	/*  --------- initial ----------- */
+	// connect discord bot:
+	discord.Connect()
+
+	//download json file
+	file := download()
+	parseJson(&file)
+
+}
+
+func download() []byte {
+
+	//temp:
+
+	file, err := os.ReadFile("temp.json")
+
+	if err != nil {
+		fmt.Println("err in opening file: ", err)
+		return nil
+	}
+
+	// res, err := http.Get("https://github.com/arkadiyt/bounty-targets-data/blob/main/data/hackerone_data.json")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// defer res.Body.Close()
+	// file, err2 := io.ReadAll(res.Body)
+	// if err != nil {
+	// 	log.Fatal(err2)
+	// }
+	return file
+}
+
+func parseJson(file *[]byte) {
+	var temp []structure.Program
+	err2 := json.Unmarshal(*file, &temp)
+	if err2 != nil {
+		fmt.Println("err2 is: ", err2)
+	}
+
+	for _, v := range temp {
+
+		res := db.FandU(v.Name, v.Target.InScope)
+		if !res {
+			if err := db.AddProgram(&v); err != nil {
+				fmt.Println("new one add: ", v.Name)
+			}
+
+		}
+
+	}
+}
