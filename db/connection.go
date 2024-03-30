1423806446ce9f594862f45a7491b8a22803baf6
blumid <shhapip13@gmail.com>
2024-03-30 16:27:46 +0330

    renew - day4, add existDB func

--- db/connection.go
+++ db/connection.go
@@ -4,27 +4,49 @@ import (
 	"context"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var collection_program, collection_domain *mongo.Collection
 var ctx = context.TODO()
+var DBExists = false
+
+// connection string
+const url = "mongodb://localhost:27017/"
 
 func init() {
-	//connection string
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
 
+	clientOptions := options.Client().ApplyURI(url)
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
+	err1 := client.Ping(ctx, nil)
+	if err1 != nil {
+		log.Fatal(err1)
+		return
+	}
+
+	//check db existence?
+	if existDB(client, "gowatch") {
+		DBExists = true
 	}
 
 	collection_program = client.Database("gowatch").Collection("programs")
+
 	// collection_domain = client.Database("gowatch").Collection("domains")
+}
+
+func existDB(client *mongo.Client, name string) bool {
+	databases, _ := client.ListDatabaseNames(context.Background(), bson.M{"name": name})
+
+	for _, db := range databases {
+		if db == name {
+			return true
+		}
+	}
+	return false
 }
