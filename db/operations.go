af1bf53c2cbb2bf7d4ce8b94f83eeedde1b9c883
blumid <shhapip13@gmail.com>
2024-03-31 13:13:03 +0330

    renew - day6,remove FandU func

--- db/operations.go
+++ db/operations.go
@@ -71,65 +71,6 @@ func ScopeDiff(new, old []structure.InScope) []structure.InScope {
 	return diff
 }
 
-func FandU(name string, array []structure.InScope) bool {
-
-	/*
-		Find program
-			- if it already exsits find get list
-			- make diff between that and db
-			- if not add it to db.
-	*/
-
-	// creating stages
-	matchStage := bson.D{{Key: "$match", Value: bson.D{{Key: "name", Value: name}}}}
-	projection := bson.D{{Key: "$project", Value: bson.M{
-		"in": bson.M{
-			"$filter": bson.M{
-				"input": "$target.inscope",
-				"as":    "elem",
-				"cond":  bson.M{"$in": []interface{}{"$$elem.assettype", bson.A{"URL", "CIDR"}}},
-			},
-		},
-		"name": 1,
-		"_id":  0},
-	},
-	}
-
-	pipeline := mongo.Pipeline{matchStage, projection}
-
-	cursor, err := collection_program.Aggregate(context.Background(), pipeline)
-	if err != nil {
-		log.Fatal("err1: ", err)
-	}
-
-	type result struct {
-		Inscopes []structure.InScope `bson:"in,omitempty" json:"in,omitempty"`
-		Name     string              `bson:"name,omitempty" json:"name,omitempty"`
-	}
-	defer cursor.Close(context.TODO())
-
-	var has bool = false
-
-	for cursor.Next(context.Background()) {
-
-		has = true
-		var res result
-		if err := cursor.Decode(&res); err != nil {
-			log.Fatal("err2: ", err)
-		}
-		diff := ScopeDiff(array, res.Inscopes)
-
-		// update using this diff thing:
-		update := bson.M{"$addToSet": bson.M{"target.inscope": bson.M{"$each": diff}}, "$set": bson.M{"updatedat": primitive.NewDateTimeFromTime(time.Now())}}
-		filter := bson.M{"name": name}
-		collection_program.UpdateOne(context.Background(), filter, update)
-		// fmt.Println("diff is: ", diff)
-
-		// fmt.Println("doc :", res.Inscopes)
-	}
-	return has
-}
-
 func AddSub(domain *structure.Domain) error {
 	data, _ := bson.Marshal(domain)
 
